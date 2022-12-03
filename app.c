cfbe0f2bfc234f44022d14d579029df5dc4239a7
June222 <38900003+June222@users.noreply.github.com>
2022-12-03 21:19:31 +0900

    feat : global variables

--- app.c
+++ app.c
@@ -114,3 +114,59 @@ static void AppTask_USART(void* p_arg);
 // Setup GPIO Port
 static void Setup_Gpio(void);
 
+
+/*
+*********************************************************************************************************
+*                                       LOCAL GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+
+/* ----------------- APPLICATION GLOBALS -------------- */
+static  OS_TCB   AppTaskStartTCB;
+static  CPU_STK  AppTaskStartStk[APP_CFG_TASK_START_STK_SIZE];
+
+// Motor Task TCB
+static  OS_TCB       Task_Rolling_TCB;
+static  OS_TCB       Task_Yawing_TCB;
+static  OS_TCB       Task_Pitching_TCB;
+
+// LED Task TCB
+static  OS_TCB       Task_LED_TCB;
+
+// USART Task TCB
+static  OS_TCB       Task_USART_TCB;
+
+// Stack for Motor Task
+static  CPU_STK  Task_Rolling_Stack[APP_CFG_TASK_START_STK_SIZE];
+static  CPU_STK  Task_Yawing_Stack[APP_CFG_TASK_START_STK_SIZE];
+static  CPU_STK  Task_Pitching_Stack[APP_CFG_TASK_START_STK_SIZE];
+
+// Stack for LED Task
+static  CPU_STK  Task_LED_Stack[APP_CFG_TASK_START_STK_SIZE];
+
+// Stack for USART Task
+static  CPU_STK  Task_USART[APP_CFG_TASK_START_STK_SIZE];
+
+task_t cyclic_tasks[TASK_N] = {
+   {"Task_Rolling" , AppTask_Rolling,	1, &Task_Rolling_Stack[0] ,	&Task_Rolling_TCB},
+   {"Task_Yawing",	 AppTask_Yawing,	1, &Task_Yawing_Stack[0],	&Task_Yawing_TCB},
+   {"Task_Pitching", AppTask_Pitching,	1, &Task_Pitching_Stack[0], &Task_Pitching_TCB},
+   {"Task_LED",		 AppTask_LED,		1, &Task_LED_Stack[0],		&Task_LED_TCB},
+   {"Task_USART",	 AppTask_USART,		0, &Task_USART[0],			&Task_USART_TCB}
+};
+
+//typedef struct {
+//    command cmd;
+//    motor_name m_name;
+//    flight_dir dir;
+//    led_cmd l_cmd;
+//}task_cmd;
+
+task_cmd motor_task_arr[MOTOR_N] = {
+    {MOTOR, ROLLING,  UP, OFF},
+    {MOTOR, YAWING,   UP, OFF},
+    {MOTOR, PITCHING, UP, OFF}
+};
+
+task_cmd led_task_arr = { LED,MOTOR_N,DIRECTION_N,OFF };
+
