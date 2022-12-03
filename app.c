5f2fe0fe39e64d535d7c53323609342b1da5ae5b
June222 <38900003+June222@users.noreply.github.com>
2022-12-03 21:19:55 +0900

    feat : main function

--- app.c
+++ app.c
@@ -170,3 +170,58 @@ task_cmd motor_task_arr[MOTOR_N] = {
 
 task_cmd led_task_arr = { LED,MOTOR_N,DIRECTION_N,OFF };
 
+
+/* ------------ FLOATING POINT TEST TASK -------------- */
+/*
+*********************************************************************************************************
+*                                                main()
+*
+* Description : This is the standard entry point for C code.  It is assumed that your code will call
+*               main() once you have performed all necessary initialization.
+*
+* Arguments   : none
+*
+* Returns     : none
+*********************************************************************************************************
+*/
+
+int main(void)
+{
+    OS_ERR  err;
+
+    /* Basic Init */
+    RCC_DeInit();
+
+    // SystemCoreClockUpdate();
+    Setup_Gpio();
+
+    /* BSP Init */
+    BSP_IntDisAll();                                            /* Disable all interrupts.                              */
+
+    CPU_Init();                                                 /* Initialize the uC/CPU Services                       */
+    Mem_Init();                                                 /* Initialize Memory Management Module                  */
+    Math_Init();                                                /* Initialize Mathematical Module                       */
+
+    /* OS Init */
+    OSInit(&err);                                               /* Init uC/OS-III.                                      */
+
+    OSTaskCreate((OS_TCB*)&AppTaskStartTCB,              /* Create the start task                                */
+        (CPU_CHAR*)"App Task Start",
+        (OS_TASK_PTR)AppTaskStart,
+        (void*)0u,
+        (OS_PRIO)APP_CFG_TASK_START_PRIO,
+        (CPU_STK*)&AppTaskStartStk[0u],
+        (CPU_STK_SIZE)AppTaskStartStk[APP_CFG_TASK_START_STK_SIZE / 10u],
+        (CPU_STK_SIZE)APP_CFG_TASK_START_STK_SIZE,
+        (OS_MSG_QTY)0u,
+        (OS_TICK)0u,
+        (void*)0u,
+        (OS_OPT)(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+        (OS_ERR*)&err
+    );
+
+    OSStart(&err);   /* Start multitasking (i.e. give control to uC/OS-III). */
+
+    (void)&err;
+    return (0u);
+}
